f3d04cbf95ccd1eb996e4e288c077ac7674437c6
Hanna Hivoina <hanna.hivoina@gmail.com>
2025-07-05 22:53:24 +0100

    Added player, asteroid and collision detection

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
+import sys
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from circleshape import CircleShape
+
+pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -11,13 +15,30 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, drawable, updatable)
+    AsteroidField.containers = (updatable,)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    asteroid = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(0, None, 0)
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.detect_collision(player):
+                print("Game Over")
+                sys.exit()
+        for drawable_object in drawable:
+            drawable_object.draw(screen)
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
